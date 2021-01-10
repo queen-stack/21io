6d0926875ec152e23232884dca9adf8c8a39d46b
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-09 21:34:52 -0600

    added default movie images

--- client/src/components/Slideshow/index.js
+++ client/src/components/Slideshow/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
+import pinocchio from '../../images/pinocchio.jpg'
+import wonderWoman from '../../images/wonderWoman.jpg';
+import newOTWorld from '../../images/newsOTWorld.jpg';
+import monsterHunter from '../../images/monsterHunter.jpg';
+
 const slideImages = [
-  'images/slide_2.jpg',
-  'images/slide_3.jpg',
-  'images/slide_4.jpg'
+  pinocchio,
+  wonderWoman,
+  newOTWorld,
+  monsterHunter
 ];
 
 const Slideshow = () => {
@@ -24,6 +30,11 @@ const Slideshow = () => {
           </div>
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
               <span>Slide 3</span>
             </div>
           </div>
