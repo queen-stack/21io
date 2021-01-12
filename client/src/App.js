74577bcc6d5fa0cf261933f4422ad0c95ff2f36d
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-12 11:34:11 -0600

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Slideshow from '../src/components/Slideshow';
+import Navbar from '../src/components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Slideshow />
     </div>
   );
