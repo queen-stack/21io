4c053c0c0df97284cfa8802de409f1bb6356228d
natswatch <nathaliec@me.com>
2021-01-17 12:51:58 -0800

    cleanup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import { useMutation } from '@apollo/react-hooks';
-import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations"
 import Auth from "../utils/auth";
 import GoogleSignIn from '../components/GoogleSignIn';
-// import GoogleSignOut from '../components/GoogleSignOut';
-
 import Button from '@material-ui/core/Button';
 
 import Navbar from '../components/Navbar';
