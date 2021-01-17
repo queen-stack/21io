6cf29a3b7014d74b7160c581423079270fc0e64e
natswatch <nathaliec@me.com>
2021-01-16 19:26:32 -0800

    Update mutations.js

--- client/src/components/utils/mutations.js
+++ client/src/components/utils/mutations.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
-export const SAVE_BOOK = gql`
+export const ADD_MOVIE = gql`
     mutation saveMovie($movieId: ID!, $description: Sting!, $name: String!, $image: String!) {
-        saveBook(movieId: $movieId, description: $description, name: $name, image: $image) {
+        saveMovie(movieId: $movieId, description: $description, name: $name, image: $image) {
             _id
         }
     }
