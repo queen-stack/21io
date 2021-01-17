5c4326aaf0d58e2db70eb4fdd29d4ad60d86bdb7
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-17 10:33:18 -0600

    changed the slider image cards

--- client/src/components/Slideshow/index.js
+++ client/src/components/Slideshow/index.js
@@ -3,16 +3,16 @@ import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
 // import sliderBackground from '../../images/sliderBackground.jpeg';
-import pinocchio from '../../images/pinocchio.jpg'
-import wonderWoman from '../../images/wonderWoman.jpg';
-import newOTWorld from '../../images/newsOTWorld.jpg';
-import monsterHunter from '../../images/monsterHunter.jpg';
+import sliderImageOne from '../../images/sliderImageOne.jpg'
+import sliderImageTwo from '../../images/sliderImageTwo.jpg';
+import sliderImageThree from '../../images/sliderImageThree.jpg';
+import sliderImageFour from '../../images/sliderImageFour.jpg';
 
 const slideImages = [
-  pinocchio,
-  wonderWoman,
-  newOTWorld,
-  monsterHunter
+  sliderImageOne,
+  sliderImageTwo,
+  sliderImageThree,
+  sliderImageFour
 ];
 
 const Slideshow = () => {
@@ -23,7 +23,7 @@ const Slideshow = () => {
             <div className='sliderImage' style={{
               'backgroundImage': `url(${slideImages[0]})`,
               'height': '30rem',
-              'width': '20rem',
+              'width': '60rem',
               'marginLeft': 'auto',
               'marginRight': 'auto',
               'marginTop': '5rem',
@@ -37,7 +37,7 @@ const Slideshow = () => {
             <div className='sliderImage' style={{
               'backgroundImage': `url(${slideImages[1]})`,
               'height': '30rem',
-              'width': '20rem',
+              'width': '60rem',
               'marginLeft': 'auto',
               'marginRight': 'auto',
               'marginTop': '5rem',
@@ -51,7 +51,7 @@ const Slideshow = () => {
             <div className='sliderImage' style={{
               'backgroundImage': `url(${slideImages[2]})`,
               'height': '30rem',
-              'width': '20rem',
+              'width': '60rem',
               'marginLeft': 'auto',
               'marginRight': 'auto',
               'marginTop': '5rem',
@@ -65,7 +65,7 @@ const Slideshow = () => {
             <div className='sliderImage' style={
               {'backgroundImage': `url(${slideImages[3]})`,
                 'height': '30rem',
-                'width': '20rem',
+                'width': '60rem',
                 'marginLeft': 'auto',
                 'marginRight': 'auto',
                 'marginTop': '5rem',
